ffd5297c9a7eccf73ec8c76e991338742fa35bbe
greenBook <Andrew@mplantproductions.com>
2019-12-01 16:28:46 -0800

    added urlpatterns for crud views

--- jobs/urls.py
+++ jobs/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls import url
-from jobs.views import JobList, JobDetailView, JobCreate
+from jobs.views import JobList, JobDetailView, JobCreate, JobUpdate, JobDelete
 
 
 urlpatterns = [
     url(r'^$', JobList.as_view(), name='job-list'),
     url(r'^create/$', JobCreate.as_view(), name='job-create'),
+    url(r'^update/$', JobUpdate.as_view(), name='job-update'),
+    url(r'^(?P<slug>[-\w]+)/delete/$', JobDelete.as_view(), name='job-delete'),
     url(r'^(?P<slug>[-\w]+)/$', JobDetailView.as_view(), name="job-detail"),
 ]
