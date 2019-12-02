cfbb4f64dda7eb10a8b7b1aa0d7917ab738348fb
greenBook <Andrew@mplantproductions.com>
2019-12-01 17:02:09 -0800

    implemented edit on detail

--- jobs/urls.py
+++ jobs/urls.py
@@ -5,7 +5,7 @@ from jobs.views import JobList, JobDetailView, JobCreate, JobUpdate, JobDelete
 urlpatterns = [
     url(r'^$', JobList.as_view(), name='job-list'),
     url(r'^create/$', JobCreate.as_view(), name='job-create'),
-    url(r'^(?P<slug>[-\w]+)/update$', JobUpdate.as_view(), name='job-update'),
+    url(r'^(?P<slug>[-\w]+)/update/$', JobUpdate.as_view(), name='job-update'),
     url(r'^(?P<slug>[-\w]+)/delete/$', JobDelete.as_view(), name='job-delete'),
     url(r'^(?P<slug>[-\w]+)/$', JobDetailView.as_view(), name="job-detail"),
 ]
